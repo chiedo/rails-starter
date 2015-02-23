8786f7eec39d13b074b494cc7974102785a4c18a
Chiedo <chiedo@chie.do>
2015-02-23 14:41:02 -0500

    Basic helper and basic helper tests

--- example-app/app/helpers/cars_helper.rb
+++ example-app/app/helpers/cars_helper.rb
@@ -1,2 +1,12 @@
 module CarsHelper
+  # Concatenates two strings. Arguably not even necessary as the function name is obvious. But a good habit to comment.
+  #
+  # * *Args*    :
+  #   - +x+ -> the string that will appear first.
+  #   - +y+ -> the string that will appeor second
+  # * *Returns* :
+  #   - the concatenated string
+  def concat_strings(x, y)
+    return [x, y].join " "
+  end
 end
--- example-app/spec/helpers/cars_helper_spec.rb
+++ example-app/spec/helpers/cars_helper_spec.rb
@@ -1,14 +1,9 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the CarsHelper. For example:
-#
-# describe CarsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe CarsHelper, type: :helper do
+   describe "string concat" do
+     it "concats two strings with spaces" do
+       expect(helper.concat_strings("this","that")).to eq("this that")
+     end
+   end
 end
