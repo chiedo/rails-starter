c5c573a1c3fcb50553fc0fc7461754877fd89b50
Chiedo <chiedo@chie.do>
2015-05-27 15:08:30 -0400

    Much better gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
-var gulp = require('gulp');
-var browserify = require('gulp-browserify');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-
+var gulp       = require('gulp');
+var source     = require('vinyl-source-stream'); // Used to stream bundle for further handling
+var browserify = require('browserify');
+var watchify   = require('watchify');
+var reactify   = require('reactify');
+var concat     = require('gulp-concat');
+ 
 gulp.task('browserify', function() {
-  gulp.src('./react/app.js')
-  .pipe(browserify({transform: 'reactify'}))
-  .pipe(concat('react-bundle.js'))
-  // Only use uglify for production build. It's much slower
-  //.pipe(uglify())
+  var bundler = browserify({
+    entries: ['./react/app.js'], // Only need initial file, browserify finds the deps
+    transform: [reactify], // We want to convert JSX to normal javascript
+    debug: true, // Gives us sourcemapping
+    cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+  });
+  var watcher  = watchify(bundler);
+
+  return watcher
+  .on('update', function () { // When any files update
+    var updateStart = Date.now();
+    console.log('Updating!');
+    watcher.bundle() // Create new bundle that uses the cache for high performance
+    .pipe(source('react-bundle.js'))
+    // This is where you add uglifying etc.
+    .pipe(gulp.dest('./app/assets/javascripts/'));
+    console.log('Updated!', (Date.now() - updateStart) + 'ms');
+  })
+  .bundle() // Create the initial bundle when starting the task
+  .pipe(source('react-bundle.js'))
   .pipe(gulp.dest('./app/assets/javascripts'));
 });
 
-gulp.task('default',['browserify', 'watch']);
-
-gulp.task('watch', function() {
-  gulp.watch(['./react/**/*'], ['browserify']);
-});
+// Just running the two tasks
+gulp.task('default', ['browserify']);
