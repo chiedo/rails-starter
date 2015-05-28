6cb449e0420580218caab5c0c20b54cf7b469646
Chiedo <chiedo@chie.do>
2015-05-28 07:35:49 -0400

    Better gulpfile and es6 support

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,46 @@
-var gulp       = require('gulp');
-var source     = require('vinyl-source-stream'); // Used to stream bundle for further handling
-var browserify = require('browserify');
+'use strict';
+
+// Credits to https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
+
 var watchify   = require('watchify');
+var browserify = require('browserify');
+var gulp       = require('gulp');
+var source     = require('vinyl-source-stream');
+var buffer     = require('vinyl-buffer');
+var gutil      = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var assign     = require('lodash.assign');
 var reactify   = require('reactify');
-var concat     = require('gulp-concat');
- 
-gulp.task('browserify', function() {
-  var bundler = browserify({
-    entries: ['./react/app.js'], // Only need initial file, browserify finds the deps
-    transform: [reactify], // We want to convert JSX to normal javascript
-    debug: false, // Gives us sourcemapping
-    cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
-  });
-  var watcher  = watchify(bundler);
+var babelify   = require("babelify");
 
-  return watcher
-  .on('update', function () { // When any files update
-    var updateStart = Date.now();
-    console.log('Updating!');
-    watcher.bundle() // Create new bundle that uses the cache for high performance
+// add custom browserify options here
+var customOpts = {
+  entries: ['./react/app.js'],
+  transform: [reactify, babelify], // We want to convert JSX to normal javascript and es6 to es5
+  debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts)); 
+
+// add transformations here
+// i.e. b.transform(coffeeify);
+
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('react-bundle.js'))
-    // This is where you add uglifying etc.
-    .pipe(gulp.dest('./app/assets/javascripts/'));
-    console.log('Updated!', (Date.now() - updateStart) + 'ms');
-  })
-  .bundle() // Create the initial bundle when starting the task
-  .pipe(source('react-bundle.js'))
-  .pipe(gulp.dest('./app/assets/javascripts'));
-});
+    // optional, remove if you don't need to buffer file contents
+    .pipe(buffer())
+    // optional, remove if you dont want sourcemaps
+    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+       // Add transformation tasks to the pipeline here.
+    .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest('./app/assets/javascripts'));
+}
 
-// Just running the two tasks
-gulp.task('default', ['browserify']);
+gulp.task('default', ['js']);
