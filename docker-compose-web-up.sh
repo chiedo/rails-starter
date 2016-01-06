72a1b947631798cbf592430fb7a9f35b9f1406ea
Chiedo <chiedo@chie.do>
2016-01-06 09:27:39 -0500

    Removed Docker and Vagrant

--- docker-compose-web-up.sh
+++
@@ -1,9 +0,0 @@
-# Wait 2 seconds to make sure this is last thing in the console
-sleep 2
-
-# Get the container ID
-CONTAINER_ID="$(cat /proc/self/cgroup | grep "docker" | sed s/\\//\\n/g | tail -1)"
-# Print out the command needed to enter the docker web container
-printf "\nTo enter your Docker web container, run the below:\n------------\ndocker exec -it ${CONTAINER_ID} bash\n"
-# The below is a hackish way to make the web container run indefinitely until stopped
-tail -f /dev/null
