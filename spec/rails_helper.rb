38da6adf02b26d59a3e16b0252b21b56bb8b4505
Chiedo <chiedo@chie.do>
2017-06-03 17:25:02 -0400

    Made tests more resilient

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,4 +47,39 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # The following two are needed for devise
+  config.include Devise::TestHelpers, type: :controller
+  config.include Warden::Test::Helpers
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_framework = :mocha
+
+  config.before :all do
+    FactoryGirl.reload
+  end
+
+  # Database cleaner: to ensure that the test database is cleaned after each test.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
